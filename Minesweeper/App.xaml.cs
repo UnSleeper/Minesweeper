891c6005f13b75278575f67ab59f20626ccbdcc9
Ivan <Vinniwata@yandex.ru>
2021-05-14 04:56:56 +0700

    View and base events

--- Minesweeper/App.xaml.cs
+++ Minesweeper/App.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Minesweeper
 {
@@ -13,5 +15,11 @@ namespace Minesweeper
     /// </summary>
     public partial class App : Application
     {
+        public bool StarGame(int size, int bomb)
+        {
+            return true;
+        }
     }
+
+
 }
--- Minesweeper/MainWindow.xaml.cs
+++ Minesweeper/MainWindow.xaml.cs
@@ -25,28 +25,101 @@ namespace Minesweeper
             InitializeComponent();
         }
 
+        public bool StarGame(int size, int bomb)
+        {
+            Random rand = new Random();
+            List<List<ButtonField>> rows = new List<List<ButtonField>>();
+            for (int i = 0; i < size; i++)
+            {
+                rows.Add(new List<ButtonField>());
+                for (int j = 0; j < size; j++)
+                {
+                    ButtonField button = new ButtonField();
+                    button.Width = 15;
+                    button.Height = 15;
+                    button.Click += FieldClickEvent;
+                    button.MouseRightButtonUp += FieldLockEvent;
+                    if (rand.Next(0, size*size) < bomb)
+                    {
+                        button.isBomb = true;
+                    }
+                    rows[i].Add(button);
+                }
+            }
+            BombBox.ItemsSource = rows;
+            return true;
+        }
+
+        class ButtonField:Button
+        {
+            public bool isBomb;
+            public bool isLock;
+            public bool isOpen;
+        }
+
+        private void FieldLockEvent(object sender, MouseButtonEventArgs e)
+        {
+            if(((ButtonField)sender).isOpen) return;
+            ((ButtonField)sender).isLock = !((ButtonField)sender).isLock;
+            if(((ButtonField)sender).isLock)
+            {
+                ((ButtonField)sender).Background = new SolidColorBrush(Color.FromArgb(255, 255, 255, 0));
+            }
+            else
+            {
+                ((ButtonField)sender).Background = new SolidColorBrush(Color.FromArgb(255, 221, 221, 221));
+            }
+        }
+
+        private void FieldClickEvent(object sender, RoutedEventArgs e)
+        {
+            if (((ButtonField)sender).isLock) return;
+            if (((ButtonField)sender).isBomb)
+            {
+                LoseGame(sender);
+            }
+            else
+            {
+                ClearField(sender);
+            }
+        }
+
+        private void LoseGame(object sender)
+        {
+            ((ButtonField)sender).Content = "*";
+            BombBox.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+        }
+        private void ClearField(object sender)
+        {
+            ((ButtonField)sender).isOpen = true;
+            ((ButtonField)sender).Background = new SolidColorBrush(Color.FromArgb(255, 0, 255, 0));
+            ((ButtonField)sender).Content = CountNearbyBombs(sender);
+        }
+        private int CountNearbyBombs(object sender)
+        {
+            return 0;
+        }
+
         private void MenuItem_Reload(object sender, RoutedEventArgs e)
         {
+
             MenuItem menuItem = (MenuItem)sender;
             MessageBox.Show(menuItem.Header.ToString());
         }
 
         private void MenuItem_Easy(object sender, RoutedEventArgs e)
         {
-            MenuItem menuItem = (MenuItem)sender;
-            MessageBox.Show(menuItem.Header.ToString());
+            StarGame(9,9);
         }
 
         private void MenuItem_Normal(object sender, RoutedEventArgs e)
         {
-            MenuItem menuItem = (MenuItem)sender;
-            MessageBox.Show(menuItem.Header.ToString());
+            StarGame(16, 50);
         }
 
         private void MenuItem_Hard(object sender, RoutedEventArgs e)
         {
-            MenuItem menuItem = (MenuItem)sender;
-            MessageBox.Show(menuItem.Header.ToString());
+            StarGame(30, 270);
         }
 
         private void MenuItem_Custom(object sender, RoutedEventArgs e)
@@ -56,8 +129,7 @@ namespace Minesweeper
         }
         private void MenuItem_Exit(object sender, RoutedEventArgs e)
         {
-            MenuItem menuItem = (MenuItem)sender;
-            MessageBox.Show(menuItem.Header.ToString());
+            Close();
         }
     }
 }
